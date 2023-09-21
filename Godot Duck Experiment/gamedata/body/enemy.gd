e5bd18a46fa2c881abefc404642378f6e7084f55
Jakub Schwarz <jschwarz@plutoniasoftware.eu>
2023-09-21 19:57:08 +0200

    [ADD] Duck play animation

--- Godot Duck Experiment/gamedata/body/enemy.gd
+++ Godot Duck Experiment/gamedata/body/enemy.gd
@@ -1,5 +1,12 @@
 extends RigidBody2D
 
+
+@onready var animsprite = $AnimatedSprite2D
+
+
+func _process(delta):
+	animsprite.play()
+
 func _physics_process(delta):
 	position.x += -200 * delta
 	if global_position.x <= -200:
